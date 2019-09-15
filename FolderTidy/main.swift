072760e5c8a0b366f0b062fa4933dc7cdd6b1b92
Michael Critz <github@michaelcritz.com>
2019-09-15 09:51:25 -0700

    Initial Commit

---
+++ FolderTidy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FolderTidy
+//
+//  Created by Michael Critz on 9/15/19.
+//  Copyright © 2019 Map of the Unexplored. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
