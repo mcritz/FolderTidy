04990aa67dbd3542c122398673305c5ce8d880a8
Michael Critz <michael.critz@sony.com>
2019-09-17 16:36:56 -0700

    code sanitization

--- Sources/FolderTidy/main.swift
+++ Sources/FolderTidy/main.swift
@@ -1,88 +1,82 @@
-/*
- As a person with a Mac
- I want a way to move all Applications from the ~/Downloads folder to the /Applications folder
- So I don’t have to do repedative tasks
- */
-
 import Foundation
 
-// 1. Get ~/Downloads folder
+// TODO: Tool should be more configuration based. Too many hardcoded values.
+
 let homeURL = FileManager.default.homeDirectoryForCurrentUser
 
+// FIXME: not localized
 let downloadsURL = homeURL.appendingPathComponent("Downloads", isDirectory: true)
 
-// 2. Find all the files
 let allFileURLs = try FileManager
     .default
     .contentsOfDirectory(at: downloadsURL,
                     includingPropertiesForKeys: [.isApplicationKey],
                     options: .skipsPackageDescendants)
 
+/// Error cases
 enum TidyError: Error {
     case invalidParameter, failedToMoveFile, failedToDeleteFile
-    var description: String {
+    var localizedDescription: String {
         get {
             switch self {
             case .invalidParameter:
-                return "Did not include a valid url or path extension"
+                return NSLocalizedString("Did not include a valid url or path extension", comment: "")
             case .failedToMoveFile:
-                return "Couldn’t move file"
+                return NSLocalizedString("Couldn’t move file", comment: "")
             case .failedToDeleteFile:
-                return "Couldn’t delete file"
+                return NSLocalizedString("Couldn’t delete file", comment: "")
             }
         }
     }
 }
 
+/// Filters files
+/// - Parameter urls: urls for filtering
+/// - Parameter pathExtensions: extensions to match
 func filterFiles(at urls: [URL], with pathExtensions: [String]) throws -> [URL] {
     guard urls.count > 0
         && pathExtensions.count > 0 else {
             throw TidyError.invalidParameter
     }
-    var matchedURLs = [URL]()
-    for pathx in pathExtensions {
-        let newMatchedURLs = urls.filter { url in
-            url.pathExtension == pathx
-        }
-        matchedURLs.append(contentsOf: newMatchedURLs)
+    return pathExtensions.flatMap { pathx in
+        urls.filter { $0.pathExtension == pathx }
     }
-    return matchedURLs
 }
 
+/// Moves files
+/// - Parameter urls: urls for candidate files
+/// - Parameter pathExtensions: extensions, like `jpg` that files must have to be moved
+/// - Parameter directory: destination
 func moveFiles(at urls: [URL], with pathExtensions: [String], to directory: URL) throws {
     let matches = try filterFiles(at: urls, with: pathExtensions)
     for fileURL in matches {
         let fileName = fileURL.lastPathComponent
-        let destinationURL = applicationsURL.appendingPathComponent(fileName)
+        let destinationURL = directory.appendingPathComponent(fileName)
         do {
             try FileManager.default.moveItem(at: fileURL,
-                                         to: destinationURL)
+                                             to: destinationURL)
         } catch {
-            print("Could not move \(fileName)")
-            print(error.localizedDescription)
             throw TidyError.failedToMoveFile
         }
     }
 }
 
+/// Deletes files
+/// - Parameter urls: urls for candidate files
+/// - Parameter pathExtensions: extensions, like `zip` that files must have to be deleted
 func deleteFiles(at urls: [URL], with pathExtensions: [String]) throws {
     let matches = try filterFiles(at: urls, with: pathExtensions)
     for fileURL in matches {
         do {
             try FileManager.default.removeItem(atPath: fileURL.path)
         } catch {
-            print("Could not remove \(fileURL.lastPathComponent)")
-            print(error.localizedDescription)
             throw TidyError.failedToDeleteFile
         }
     }
 }
 
-
-// 
-
-let applicationsURL = URL(fileURLWithPath: "/Applications",
-isDirectory: true)
+// TODO: Configuration points
+let applicationsURL = URL(fileURLWithPath: "/Applications", isDirectory: true)
 
 try moveFiles(at: allFileURLs, with: ["app"], to: applicationsURL)
 
